e9a1baa6879eb616f2e995f080f7300b0b4e3c3e
Alex Nudelman <nudelx@gmail.com>
2016-02-11 00:58:32 +0200

    added basic table

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Component } from 'react';
 
-import SearchBar from '../containers/search_bar';
+import SearchBar  from '../containers/search_bar';
+import WetherList from '../containers/weather_list';
+
 
 export default class App extends Component {
   render() {
     return (
+    	<div>
       	<SearchBar />
+      	<WetherList />
+      </div>
     );
   }
 }
