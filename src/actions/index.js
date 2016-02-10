523df025903b530707bcaae96f80a246b583874f
Alex Nudelman <nudelx@gmail.com>
2016-02-10 23:21:45 +0200

    added connect to bind the action with search bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '52d1fd906fa8c5b9333b56edb08bad69';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,10 +7,13 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
 
-	const url = `${ROOT_URL}&q=${city},us`
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
 
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
+		payload: request
+
 	}
 
 }
