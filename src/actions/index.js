8b6f7a04a85d672ce226e959d5e13be1af2f4c52
Alex Nudelman <nudelx@gmail.com>
2016-02-10 23:03:42 +0200

    added redux promise

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+const API_KEY = '52d1fd906fa8c5b9333b56edb08bad69';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+
+	const url = `${ROOT_URL}&q=${city},us`
+
+	return {
+		type: FETCH_WEATHER
+	}
+
+}
